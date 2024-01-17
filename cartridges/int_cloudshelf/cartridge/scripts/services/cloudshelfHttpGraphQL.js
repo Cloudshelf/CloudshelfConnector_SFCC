b17fd107e9278b504826e8e6a208b2fe48b8bffa
Maksym Stefanko <123931942+maksymstefankoct@users.noreply.github.com>
2024-01-17 11:37:18 +0200

    GraphQL types and service data and structure (#2)
    * Added graphql types and script for queries
    
    * Added service and init cloudshelf api model
    
    * added product group query type

---
+++ cartridges/int_cloudshelf/cartridge/scripts/services/cloudshelfHttpGraphQL.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const Site = require('dw/system/Site');
+const LocalServiceRegistry = require('dw/svc/LocalServiceRegistry');
+
+const serviceName = 'cloudshelf.http.graphql';
+
+/**
+ * Returns Cloudshelf API key value
+ * @returns {string} api key value
+ * @throws {Error} If API key is not found
+ * @private
+ */
+function getAPIKey() {
+    const apiKey = Site.getCurrent().getCustomPreferenceValue('cloudshelfAPIKey');
+    if (!apiKey) {
+        throw new Error('Cloudshelf API key site preference value is empty');
+    }
+    return apiKey;
+}
+
+/**
+ * Returns an object with the service's callback functions implemented
+ * @returns {Object} service config object
+ */
+function getCloudshelfGraphQLServiceConfig() {
+    return {
+        createRequest: function (service, requestData) {
+            service.addHeader('Authorization', 'Token ' + getAPIKey());
+            service.addHeader('Content-Type', 'application/json');
+
+            return JSON.stringify(requestData);
+        },
+        parseResponse: function (svc, httpClient) {
+            let serviceResponse;
+
+            try {
+                serviceResponse = JSON.parse(httpClient.getText());
+            } catch (error) {
+                return {
+                    error: true,
+                    errorMessage: error.message
+                };
+            }
+
+            if (!serviceResponse || !serviceResponse.data) {
+                return {
+                    error: true
+                };
+            }
+
+            return {
+                error: false,
+                data: serviceResponse.data
+            };
+        },
+        filterLogMessage: function (msg) {
+            return msg;
+        }
+    };
+}
+
+module.exports = function () {
+    return LocalServiceRegistry.createService(serviceName, getCloudshelfGraphQLServiceConfig());
+};
